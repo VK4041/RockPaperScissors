c9c579d0cad3c7822e68f152dd34b6281a822641
VK4041 <151887513+VK4041@users.noreply.github.com>
2025-08-31 15:57:27 +1000

    Add rps human choice event handlers

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ function setAttributes(node, ...attributes) {
     })
 }
 function displayStartScreen() {
+    //element declaration
     const topDiv = document.createElement('div')
     const middleDiv = document.createElement('div')
     const startDiv = document.createElement('div')
@@ -27,128 +28,154 @@ function displayStartScreen() {
     const footerImg = document.createElement('img')
     const footerLink = document.createElement('a')
 
-    topDiv.appendChild(title)
-    startDiv.appendChild(startBtn)
-    middleDiv.appendChild(startDiv)
-    appendChildren(container, topDiv, middleDiv, footerDiv)
-    footerText.textContent = `Made by Varun Kumar`
-    footerLink.appendChild(footerImg)
-    footerText.appendChild(footerLink)
-    footerDiv.appendChild(footerText)
-
+    //append children
+    {
+        topDiv.appendChild(title)
+        startDiv.appendChild(startBtn)
+        middleDiv.appendChild(startDiv)
+        appendChildren(container, topDiv, middleDiv, footerDiv)
+        footerText.textContent = `Made by Varun Kumar`
+        footerLink.appendChild(footerImg)
+        footerText.appendChild(footerLink)
+        footerDiv.appendChild(footerText)
+    }
     title.textContent = 'Rock Paper Scissors'
     startBtn.textContent = 'Start Game'
-
-    setAttributes(footerImg, ['src', './images/github.png'], ['alt', 'GitHub Icon'], ['class', 'footerImg'])
-    setAttributes(footerLink, ['href', 'https://github.com/VK4041'], ['class', 'popUp'], ['target', '_blank'])
-    addClasses(['flexedDiv'], topDiv, middleDiv, footerDiv, container)
-    addClasses(['bigTitle'], title)
-    addClasses(['footerDiv'], footerDiv)
-    addClasses(['footerText'], footerText)
-    addClasses(['startButton'], startBtn)
-    addClasses(['midDiv'], middleDiv)
-    addClasses(['startDiv'], startDiv)
-    addClasses(['center'], footerText)
+    //set attributes and classes
+    {
+        setAttributes(footerImg, ['src', './images/github.png'], ['alt', 'GitHub Icon'], ['class', 'footerImg'])
+        setAttributes(footerLink, ['href', 'https://github.com/VK4041'], ['class', 'popUp'], ['target', '_blank'])
+        addClasses(['flex-col-center'], topDiv, middleDiv, footerDiv, container)
+        addClasses(['theme'], title, startBtn)
+        addClasses(['footerDiv'], footerDiv)
+        addClasses(['footerText'], footerText)
+        addClasses(['startButton'], startBtn)
+        addClasses(['midDiv'], middleDiv)
+        addClasses(['startDiv'], startDiv)
+        addClasses(['center'], footerText)
+    }
+    startBtn.addEventListener('click', () => displayGameScreen())
 }
-displayStartScreen()
-// //This function returns a random integer between 1-3 inclusive => [1,3]
-// function getComputerChoice() {
-//     //Creating a number in range [0,3),
-//     //adding 1 makes it in range [1,4),
-//     //finally, flooring it makes it lie in range [1,3]
-//     let cpuChoice = Math.floor(Math.random() * 3 + 1)
+function displayGameScreen() {
+    //clear midDiv for next step
+    const middleDiv = document.querySelector('.midDiv')
+    while (middleDiv.firstChild) {
+        middleDiv.removeChild(middleDiv.lastChild)
+    }
+    //element declarations
+    const playArea = document.createElement('section')
+    const resultsArea = document.createElement('section')
+    const leftPane = document.createElement('section')
+    const rightPane = document.createElement('section')
+    const humanTag = document.createElement('div')
+    const choiceRow1 = document.createElement('div')
+    const choiceRow2 = document.createElement('div')
+    const cpuTag = document.createElement('div')
+    const cpuDiv = document.createElement('div')
+    const rockImg = document.createElement('img')
+    const paperImg = document.createElement('img')
+    const scissorsImg = document.createElement('img')
+    const cpuChoiceImg = document.createElement('img')
 
-//     let cpuSelection
-//     //using switch statement to return either R,P or S as cpu choice
-//     switch (cpuChoice) {
-//         case 1: cpuSelection = "ROCK"
-//             break;
-//         case 2: cpuSelection = "PAPER"
-//             break;
-//         case 3: cpuSelection = "SCISSORS"
-//             break;
-//     }
-//     console.log("Computer Choice = ", cpuSelection)
-//     return cpuSelection
-// }
-// //This function gets the human user's choice using prompt
-// function getHumanChoice() {
-//     //first, we ask the user to enter their choice
-//     let humanChoice = prompt("Enter your choice (ROCK, PAPER or SCISSORS): ")
+    //appending children
+    {
+        appendChildren(middleDiv, playArea, resultsArea)
+        appendChildren(playArea, leftPane, rightPane)
+        appendChildren(leftPane, humanTag, choiceRow1, choiceRow2)
+        appendChildren(choiceRow1, rockImg, paperImg, scissorsImg)
+        appendChildren(rightPane, cpuTag, cpuDiv)
+        appendChildren(cpuDiv, cpuChoiceImg)
+    }
+    humanTag.textContent = `YOU`
+    cpuTag.textContent = `CPU`
 
-//     //then, in case they enter an empty value or cancel the prompt, we output "TERMINATED"
-//     if (humanChoice === "" || humanChoice === null) {
-//         throw new Error("No value was entered by user, aborting game")
-//     }
-//     //else, compare
-//     else {
-//         //trim and capitalize the choice for stable comparisons
-//         humanChoice = humanChoice.trim().toUpperCase()
+    //setting attributes and adding classes
+    {
+        addClasses(['theme', 'nameTag'], humanTag, cpuTag)
+        addClasses(['flex-row-center'], playArea,)
+        addClasses(['flex-col-center'], leftPane, rightPane, resultsArea)
+        setAttributes(rockImg, ['src', './images/rock.jpg'], ['name', 'rock'])
+        setAttributes(paperImg, ['src', './images/paper.jpg'], ['name', 'paper'])
+        setAttributes(scissorsImg, ['src', './images/scissors.jpg'], ['name', 'scissors'])
+        addClasses(['rpsImages'], rockImg, paperImg, scissorsImg)
+        addClasses(['center'], choiceRow1, choiceRow2, cpuDiv)
+        addClasses(['fixed-img'], cpuChoiceImg)
+        addClasses(['alignTop'], cpuTag)
+        addClasses(['cpuChoice'], cpuDiv)
 
-//         //check if the choice is valid or not
-//         if (["ROCK", "PAPER", "SCISSORS"].includes(humanChoice)) {
-//             console.log("Human Choice = ", humanChoice)
-//             return humanChoice
-//         }
+    }
 
-//         //if not valid, then say invalid and redirect to the same function again until user enters a valid one!
-//         else {
-//             alert("Please enter a valid choice!, try again")
-//             getHumanChoice()
-//         }
-//     }
-// }
-// // Function the play a round and decide win, loss or tie and update the scores
-// function playRound(round) {
+    let images = [rockImg, paperImg, scissorsImg]
+    let humanChoice, cpuChoice;
 
-//     //Print the round number before each player makes their choice
-//     console.log(`\n\tRound ${round}`)
-//     let cpuChoice = getComputerChoice();
-//     let humanChoice = getHumanChoice();
-//     console.log(cpuChoice, humanChoice)
+    images.map(img => img.addEventListener('click', () => {
+        humanChoice = img.getAttribute('name')
+        cpuChoice = getComputerChoice()
+        decideWinner(cpuChoice, humanChoice)
+    }))
+}
 
-//     //Compare the choices of cpu and human user
 
-//     //TIE if same choice by both
-//     if (cpuChoice === humanChoice) {
-//         console.log("IT'S A TIE!!!")
-//     }
-//     //All cases where CPU wins
-//     else if ((cpuChoice === 'ROCK' && humanChoice === 'SCISSORS') || (cpuChoice === 'PAPER' && humanChoice === 'ROCK') || (cpuChoice === 'SCISSORS' && humanChoice === 'PAPER')) {
-//         cpuScore += 1
-//         console.log('You Lose!')
-//     }
-//     //All other cases, where human wins
-//     else {
-//         humanScore += 1
-//         console.log('You Win!')
-//     }
-// }
+function getComputerChoice() {
+    let cpuChoice = Math.floor(Math.random() * 3 + 1)
 
-// //Function to play the game (5 rounds and decide the overall winner)
-// function playGame() {
-//     console.log("The beginning of a new journey!")
-//     //Run 5 rounds
-//     for (let i = 1; i <= 5; i++) {
-//         playRound(i)
-//     }
-//     //Game end indicator and scoreboard
-//     console.log(`\n\t------Game Over------\n\t------Results------`)
-//     console.log(`Human Score = ${humanScore}\nCPU Score = ${cpuScore}`)
-//     if (cpuScore === humanScore) {
-//         console.log("OVERALL TIE!!!")
-//     }
-//     else if (cpuScore > humanScore) {
-//         console.log("CPU wins the whole game!!!")
-//     }
-//     else {
-//         console.log("You win the whole game!!!")
-//     }
-// }
-// //Try-Catch block to handle the exception generated on line 35
-// try {
-//     playGame()
-// }
-// catch (err) {
-//     console.log(err)
-// }
+    let cpuSelection
+    //using switch statement to return either R,P or S as cpu choice
+    switch (cpuChoice) {
+        case 1: cpuSelection = "rock"
+            break;
+        case 2: cpuSelection = "paper"
+            break;
+        case 3: cpuSelection = "scissors"
+            break;
+    }
+    return cpuSelection
+}
+
+function decideWinner(cpuChoice, humanChoice) {
+
+    console.log(`CPU: ${cpuChoice} | YOU: ${humanChoice}`)
+
+    let outcome
+    //TIE if same choice by both
+    if (cpuChoice === humanChoice) {
+        outcome = "IT'S A TIE!!!"
+    }
+    //All cases where CPU wins
+    else if ((cpuChoice === 'rock' && humanChoice === 'scissors')
+        || (cpuChoice === 'paper' && humanChoice === 'rock')
+        || (cpuChoice === 'scissors' && humanChoice === 'paper')) {
+        cpuScore += 1
+        outcome = 'You Lose!'
+    }
+    //All other cases, where human wins
+    else {
+        humanScore += 1
+        outcome = 'You Win!'
+    }
+    console.log(outcome)
+}
+
+//Function to play the game (5 rounds and decide the overall winner)
+function playGame() {
+    displayStartScreen()
+    // //Game end indicator and scoreboard
+    // console.log(`\n\t------Game Over------\n\t------Results------`)
+    // console.log(`Human Score = ${humanScore}\nCPU Score = ${cpuScore}`)
+    // if (cpuScore === humanScore) {
+    //     console.log("OVERALL TIE!!!")
+    // }
+    // else if (cpuScore > humanScore) {
+    //     console.log("CPU wins the whole game!!!")
+    // }
+    // else {
+    //     console.log("You win the whole game!!!")
+    // }
+}
+//Try-Catch block to handle the exception generated on line 35
+try {
+    playGame()
+}
+catch (err) {
+    console.log(err)
+}
